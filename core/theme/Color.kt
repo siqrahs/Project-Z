d8c075629bc7ef4dd8f4276c63bb9e95ce4f2a0f
Siqrahs <233435765+siqrahs@users.noreply.github.com>
2026-07-20 16:46:08 +0800

    Create theme/Color.kt

---
+++ core/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.open.securitymanager.core.theme
+
+import androidx.compose.ui.graphics.Color
+
+val XiaomiGreen = Color(0xFF10B981)      // Status Terhubung & Safe
+val XiaomiGreenDark = Color(0xFF047857)  
+val XiaomiRed = Color(0xFFEF4444)        // Status Tidak Terhubung & Danger
+val XiaomiOrange = Color(0xFFF59E0B)     // Warning
+val XiaomiBlue = Color(0xFF3B82F6)       // Primary Accent
+
+val CardDarkBackground = Color(0xFF1E293B)
+val AppDarkBackground = Color(0xFF0F172A)
+val TextPrimaryDark = Color(0xFFF8FAFC)
+val TextSecondaryDark = Color(0xFF94A3B8)
